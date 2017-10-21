fa19c2304618e7d0d7f6df0c431d9e68001d5c83
Austin Hwang <austin.y.hwang@gmail.com>
2017-10-21 17:27:22 -0400

    Added ability to analyze multiple photos

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,27 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
-
 var mongoose = require('mongoose');
 var config = require('../config/config');
 var ImgurImage = require('../models/ImgurImage');
 var fs = require("fs");
+
 
 var contents = fs.readFileSync(__dirname + "/images.json");
 // Define to JSON type
 var jsonContent = JSON.parse(contents);
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    var init = { 
+      images:[] 
+    };
+    fs.writeFile(__dirname + "/images.json", init, (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        };
+        console.log("File has been created");
+    });
      res.sendFile(path.join(__dirname, '..', 'index.html'));
 });
 
@@ -42,7 +52,8 @@ router.post('/image', function(req,res,next){
       if (err) throw err;
       console.log('User saved successfully!');
     });
-    
+    res.send(jsonContent);
+    console.log(jsonContent.images);
     mongoose.connection.close();    
 });
 
