4b5f95740a71d222e06b95a8775b4978b7b8914f
Austin Hwang <austin.y.hwang@gmail.com>
2017-10-22 06:53:44 -0400

    Final changes

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -39,25 +39,13 @@ function getData(){
     xhr.send();
 
 }
-   
+
 function processImage(data) {
     console.log("Value: " + parseInt(document.getElementById("inputImage").value));
     console.log("Image links: " + data[1]);
-    // **********************************************
-    // *** Update or verify the following values. ***
-    // **********************************************
 
-    // Replace the subscriptionKey string value with your valid subscription key.
     var subscriptionKey = "2445910071604e4dbb2ff57680673887";
 
-    // Replace or verify the region.
-    //
-    // You must use the same region in your REST API call as you used to obtain your subscription keys.
-    // For example, if you obtained your subscription keys from the westus region, replace
-    // "westcentralus" in the URI below with "westus".
-    //
-    // NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
-    // a free trial subscription key, you should not need to change this region.
     var uriBase = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect";
 
     // Request parameters.
@@ -155,89 +143,63 @@ function processImage(data) {
             numPeople++;
 
             if (data[i].faceAttributes.gender === "male") {
-                  male++;
-            }
-            else if (data[i].faceAttributes.gender === "female") {
-                  female++;
-            }
-        }
-            if (male > 0) {
-                  document.getElementById("gender").innerHTML = ("Gender: " + '&#x1F466;');
-            }
-            if (female > 0) {
-                document.getElementById("gender").innerHTML = ("Gender: " + '&#x1F467;');
-            }
-            if (male > 0 && female > 0)
-            {
-                document.getElementById("gender").innerHTML = ("Gender: " + '&#x1F466;' + '&#x1F467;');
-            }
+              male++;
+          }
+          else if (data[i].faceAttributes.gender === "female") {
+              female++;
+          }
+      }
+      if (male > 0) {
+          document.getElementById("gender").innerHTML = ("Gender: " + '&#x1F466;');
+      }
+      if (female > 0) {
+        document.getElementById("gender").innerHTML = ("Gender: " + '&#x1F467;');
+    }
+    if (male > 0 && female > 0)
+    {
+        document.getElementById("gender").innerHTML = ("Gender: " + '&#x1F466;' + '&#x1F467;');
+    }
 
-        happiness /= numPeople;
-        sadness /= numPeople;
-        anger /= numPeople;
-        neutral /= numPeople;
-        fear /= numPeople;
-        disgust /= numPeople;
-        contempt /= numPeople;
-        surprise /= numPeople;
-        age /= numPeople;
+    happiness /= numPeople;
+    sadness /= numPeople;
+    anger /= numPeople;
+    neutral /= numPeople;
+    fear /= numPeople;
+    disgust /= numPeople;
+    contempt /= numPeople;
+    surprise /= numPeople;
+    age /= numPeople;
 
-        document.getElementById("age").innerHTML = "Age: " + Math.round(age);
+    document.getElementById("age").innerHTML = "Age: " + Math.round(age);
 
+    hapSlide = getEmotionScore(happiness, sadness);
+    madSlide = getEmotionScore(anger, neutral);
+    console.log(data);
 
-        /* happiness += parseFloat(data[0].faceAttributes.emotion.happiness);
-        sadness += parseFloat(data[0].faceAttributes.emotion.sadness);
-        anger += parseFloat(data[0].faceAttributes.emotion.anger);
-        neutral += parseFloat(data[0].faceAttributes.emotion.neutral);
-        fear += parseFloat(data[0].faceAttributes.emotion.fear);
-        disgust += parseFloat(data[0].faceAttributes.emotion.disgust);
-        contempt += parseFloat(data[0].faceAttributes.emotion.contempt);
-        surprise += parseFloat(data[0].faceAttributes.emotion.surprise);
-        age += parseFloat(data[0].faceAttributes.age);*/
+    value(hapSlide, "happiness");
+    value(madSlide, "anger");
+    value(contempt, "contempt");
+    value(fear, "fear");
+    value(surprise, "surprise");
+    document.getElementById("maleTotal").innerHTML = "Male Participants: " + maleTotal;
+    document.getElementById("femaleTotal").innerHTML = "Female Participants: " + femaleTotal;
+    document.getElementById("avgAge").innerHTML = "Average Age: " + Math.round(ageTotal/peopleTotal);
+    document.getElementById("numPhoto").innerHTML = "Photos Taken: " + numPhotos;
+})
 
-        hapSlide = getEmotionScore(happiness, sadness);
-        madSlide = getEmotionScore(anger, neutral);
-        console.log(data);
-
-        value(hapSlide, "happiness");
-        value(madSlide, "anger");
-        value(contempt, "contempt");
-        value(fear, "fear");
-        value(surprise, "surprise");
-        document.getElementById("maleTotal").innerHTML = "Male Participants: " + maleTotal;
-        document.getElementById("femaleTotal").innerHTML = "Female Participants: " + femaleTotal;
-        document.getElementById("avgAge").innerHTML = "Average Age: " + Math.round(ageTotal/peopleTotal);
-        document.getElementById("numPhoto").innerHTML = "Photos Taken: " + numPhotos;
-    })
-
-    .fail(function(jqXHR, textStatus, errorThrown) {
+.fail(function(jqXHR, textStatus, errorThrown) {
         // Display error message.
         var errorString = (errorThrown === "") ? "Error. " : errorThrown + " (" + jqXHR.status + "): ";
         errorString += (jqXHR.responseText === "") ? "" : (jQuery.parseJSON(jqXHR.responseText).message) ? 
-            jQuery.parseJSON(jqXHR.responseText).message : jQuery.parseJSON(jqXHR.responseText).error.message;
+        jQuery.parseJSON(jqXHR.responseText).message : jQuery.parseJSON(jqXHR.responseText).error.message;
         alert(errorString);
     });
 };
 
-  function processImage2(data) {
-    // **********************************************
-    // *** Update or verify the following values. ***
-    // **********************************************
+function processImage2(data) {
 
-    // Replace the subscriptionKey string value with your valid subscription key.
     var subscriptionKey = "69cfa0f941224f0aa1041e6970f12af0";
 
-    // Replace or verify the region.
-    //
-    // You must use the same region in your REST API call as you used to obtain your subscription keys.
-    // For example, if you obtained your subscription keys from the westus region, replace
-    // "westcentralus" in the URI below with "westus".
-    //
-    // NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
-    // a free trial subscription key, you should not need to change this region.
-    //
-    // Also, if you want to use the celebrities model, change "landmarks" to "celebrities" here and in
-    // uriBuilder.setParameter to use the Celebrities model.
     var uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/models/celebrities/analyze";
 
     // Request parameters.
@@ -246,7 +208,7 @@ function processImage(data) {
     };
 
     // Display the image.
-   var sourceImageUrl = data.images[parseInt(document.getElementById("inputImage").value) - 1]; 
+    var sourceImageUrl = data.images[parseInt(document.getElementById("inputImage").value) - 1]; 
     //var sourceImageUrl = document.getElementById("inputImage").value;
     document.querySelector("#sourceImage").src = sourceImageUrl;
 
@@ -285,276 +247,3 @@ function processImage(data) {
         alert(errorString);
     });
 };
-/*
-function getDataAll(index){        
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://localhost:3000/image', true);
-    xhr.onload = function (e) {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                console.log(xhr.responseText);
-                allImages = JSON.parse(xhr.responseText);
-            }
-        }
-        processImageAll(allImages.images[index]);
-        processImage2All(allImages.images[index]);
-    };
-    xhr.send();
-
-}
-
-/*
-function analyzeAll(data){
-    console.log("Entered analyzeAll");
-    for(var j = 0; j < allImages.length; j++)
-    {
-        getDataAll(j);
-
-        for (var i = 0; i < data.length; i++)
-        {
-            peopleTotal++;
-            happinessTotal += parseFloat(data[i].faceAttributes.emotion.happiness)/peopleTotal;
-            sadnessTotal += parseFloat(data[i].faceAttributes.emotion.sadness)/peopleTotal;
-            angerTotal += parseFloat(data[i].faceAttributes.emotion.anger)/peopleTotal;
-            neutralTotal += parseFloat(data[i].faceAttributes.emotion.neutral)/peopleTotal;
-            fearTotal += parseFloat(data[i].faceAttributes.emotion.fear)/peopleTotal;
-            disgustTotal += parseFloat(data[i].faceAttributes.emotion.disgust)/peopleTotal;
-            contemptTotal += parseFloat(data[i].faceAttributes.emotion.contempt)/peopleTotal;
-            surpriseTotal += parseFloat(data[i].faceAttributes.emotion.surprise)/peopleTotal;
-            ageTotal += parseFloat(data[i].faceAttributes.age)/peopleTotal;
-            if (data[i].faceAttributes.gender === "male") {
-                maleTotal++;
-            }
-            else if (data[i].faceAttributes.gender === "female") {
-                femaleTotal++;
-            }
-        }
-        hapSlideTotal = getEmotionScore(happinessTotal, sadnessTotal);
-        madSlideTotal = getEmotionScore(angerTotal, neutralTotal);
-
-        value(hapSlideTotal, "happinessTotal");
-        value(madSlideTotal, "angerTotal");
-        value(contemptTotal, "contemptTotal");
-        value(fearTotal, "fearTotal");
-        value(surpriseTotal, "surpriseTotal");
-    }
-
-
-}
-
-function processImageAll(data) {
-    console.log("Value: " + parseInt(document.getElementById("inputImage").value));
-    console.log("Image links: " + data[1]);
-    // **********************************************
-    // *** Update or verify the following values. ***
-    // **********************************************
-
-    // Replace the subscriptionKey string value with your valid subscription key.
-    var subscriptionKey = "2445910071604e4dbb2ff57680673887";
-
-    // Replace or verify the region.
-    //
-    // You must use the same region in your REST API call as you used to obtain your subscription keys.
-    // For example, if you obtained your subscription keys from the westus region, replace
-    // "westcentralus" in the URI below with "westus".
-    //
-    // NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
-    // a free trial subscription key, you should not need to change this region.
-    var uriBase = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect";
-
-    // Request parameters.
-    var params = {
-        "returnFaceId": "true",
-        "returnFaceLandmarks": "false",
-        "returnFaceAttributes": "age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise",
-    };
-    //var images = [];
-
-    var sourceImageUrl = data; 
-
-    console.log("Source Image Url: " + sourceImageUrl);
-
-    // Display the image.
-    //var sourceImageUrl = document.getElementById("inputImage").value;
-    document.querySelector("#sourceImage").src = sourceImageUrl;
-
-    // Perform the REST API call.
-    $.ajax({
-        url: uriBase + "?" + $.param(params),
-
-        // Request headers.
-        beforeSend: function(xhrObj){
-            xhrObj.setRequestHeader("Content-Type","application/json");
-            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
-        },
-
-        type: "POST",
-
-        // Request body.
-        data: '{"url": ' + '"' + sourceImageUrl + '"}',
-    })
-
-    .done(function(data) {
-        // Show formatted JSON on webpage.
-        $("#responseTextArea").val(JSON.stringify(data, null, 2));
-
-        for (var i = 0; i < data.length; i++)
-        {
-            peopleTotal++;
-            happinessTotal += parseFloat(data[i].faceAttributes.emotion.happiness)/peopleTotal;
-            sadnessTotal += parseFloat(data[i].faceAttributes.emotion.sadness)/peopleTotal;
-            angerTotal += parseFloat(data[i].faceAttributes.emotion.anger)/peopleTotal;
-            neutralTotal += parseFloat(data[i].faceAttributes.emotion.neutral)/peopleTotal;
-            fearTotal += parseFloat(data[i].faceAttributes.emotion.fear)/peopleTotal;
-            disgustTotal += parseFloat(data[i].faceAttributes.emotion.disgust)/peopleTotal;
-            contemptTotal += parseFloat(data[i].faceAttributes.emotion.contempt)/peopleTotal;
-            surpriseTotal += parseFloat(data[i].faceAttributes.emotion.surprise)/peopleTotal;
-            ageTotal += parseFloat(data[i].faceAttributes.age)/peopleTotal;
-            if (data[i].faceAttributes.gender === "male") {
-                maleTotal++;
-            }
-            else if (data[i].faceAttributes.gender === "female") {
-                femaleTotal++;
-            }
-        }
-        hapSlideTotal = getEmotionScore(happinessTotal, sadnessTotal);
-        madSlideTotal = getEmotionScore(angerTotal, neutralTotal);
-
-        value(hapSlideTotal, "happinessTotal");
-        value(madSlideTotal, "angerTotal");
-        value(contemptTotal, "contemptTotal");
-        value(fearTotal, "fearTotal");
-        value(surpriseTotal, "surpriseTotal");
-
-        var happiness = 0;
-        var sadness = 0;
-        var anger = 0;
-        var neutral = 0;
-        var fear = 0;
-        var disgust = 0;
-        var contempt = 0;
-        var surprise = 0;
-        var age = 0;
-        var gender = 0;
-        var hapSlide = 0;
-        var madSlide = 0;
-        var numPeople = 0;
-
-        for (var i = 0; i < data.length; i++)
-        {
-            happiness += parseFloat(data[i].faceAttributes.emotion.happiness);
-            sadness += parseFloat(data[i].faceAttributes.emotion.sadness);
-            anger += parseFloat(data[i].faceAttributes.emotion.anger);
-            neutral += parseFloat(data[i].faceAttributes.emotion.neutral);
-            fear += parseFloat(data[i].faceAttributes.emotion.fear);
-            disgust += parseFloat(data[i].faceAttributes.emotion.disgust);
-            contempt += parseFloat(data[i].faceAttributes.emotion.contempt);
-            surprise += parseFloat(data[i].faceAttributes.emotion.surprise);
-            age += parseFloat(data[i].faceAttributes.age);
-            numPeople++;
-        }
-
-        happiness /= numPeople;
-        sadness /= numPeople;
-        anger /= numPeople;
-        neutral /= numPeople;
-        fear /= numPeople;
-        disgust /= numPeople;
-        contempt /= numPeople;
-        surprise /= numPeople;
-        age /= numPeople;
-
-        /* happiness += parseFloat(data[0].faceAttributes.emotion.happiness);
-        sadness += parseFloat(data[0].faceAttributes.emotion.sadness);
-        anger += parseFloat(data[0].faceAttributes.emotion.anger);
-        neutral += parseFloat(data[0].faceAttributes.emotion.neutral);
-        fear += parseFloat(data[0].faceAttributes.emotion.fear);
-        disgust += parseFloat(data[0].faceAttributes.emotion.disgust);
-        contempt += parseFloat(data[0].faceAttributes.emotion.contempt);
-        surprise += parseFloat(data[0].faceAttributes.emotion.surprise);
-        age += parseFloat(data[0].faceAttributes.age);
-
-        hapSlide = getEmotionScore(happiness, sadness);
-        madSlide = getEmotionScore(anger, neutral);
-        console.log(data);
-
-        value(hapSlide, "happiness");
-        value(madSlide, "anger");
-        value(contempt, "contempt");
-        value(fear, "fear");
-        value(surprise, "surprise");
-    })
-
-    .fail(function(jqXHR, textStatus, errorThrown) {
-        // Display error message.
-        var errorString = (errorThrown === "") ? "Error. " : errorThrown + " (" + jqXHR.status + "): ";
-        errorString += (jqXHR.responseText === "") ? "" : (jQuery.parseJSON(jqXHR.responseText).message) ? 
-            jQuery.parseJSON(jqXHR.responseText).message : jQuery.parseJSON(jqXHR.responseText).error.message;
-        alert(errorString);
-    });
-};
-
-  function processImage2All(data) {
-    // **********************************************
-    // *** Update or verify the following values. ***
-    // **********************************************
-
-    // Replace the subscriptionKey string value with your valid subscription key.
-    var subscriptionKey = "69cfa0f941224f0aa1041e6970f12af0";
-
-    // Replace or verify the region.
-    //
-    // You must use the same region in your REST API call as you used to obtain your subscription keys.
-    // For example, if you obtained your subscription keys from the westus region, replace
-    // "westcentralus" in the URI below with "westus".
-    //
-    // NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
-    // a free trial subscription key, you should not need to change this region.
-    //
-    // Also, if you want to use the celebrities model, change "landmarks" to "celebrities" here and in
-    // uriBuilder.setParameter to use the Celebrities model.
-    var uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/models/celebrities/analyze";
-
-    // Request parameters.
-    var params = {
-        "model": "celebrities", // Use "model": "celebrities" to use the Celebrities model.
-    };
-
-    // Display the image.
-   var sourceImageUrl = data; 
-    //var sourceImageUrl = document.getElementById("inputImage").value;
-    document.querySelector("#sourceImage").src = sourceImageUrl;
-
-    // Perform the REST API call.
-    $.ajax({
-        url: uriBase + "?" + $.param(params),
-
-        // Request headers.
-        beforeSend: function(xhrObj) {
-            xhrObj.setRequestHeader("Content-Type", "application/json");
-            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
-        },
-
-        type: "POST",
-
-        // Request body.
-        data: '{"url": ' + '"' + sourceImageUrl + '"}',
-    })
-
-    .done(function(data) {
-        // Show formatted JSON on webpage.
-        $("#responseTextArea2").val(JSON.stringify(data, null, 2));
-        if (data.result.celebrities[0].name)
-        {
-            document.getElementById("numCeleb").innerHTML = data.result.celebrities[0].name + " ";
-        }
-    })
-
-    .fail(function(jqXHR, textStatus, errorThrown) {
-        // Display error message.
-        var errorString = (errorThrown === "") ? "Error. " : errorThrown + " (" + jqXHR.status + "): ";
-        errorString += (jqXHR.responseText === "") ? "" : jQuery.parseJSON(jqXHR.responseText).message;
-        alert(errorString);
-    });
-};*/
-
--- routes/index.js
+++ routes/index.js
@@ -8,34 +8,34 @@ var fs = require("fs");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    var init = { 
-      images:[] 
+  var init = { 
+    images:[] 
+  };
+  fs.writeFile(__dirname + "/images.json", JSON.stringify(init, null, 4), (err) => {
+    if (err) {
+      console.error(err);
+      return;
     };
-    fs.writeFile(__dirname + "/images.json", JSON.stringify(init, null, 4), (err) => {
-        if (err) {
-            console.error(err);
-            return;
-        };
-        console.log("File has been created");
-    });
-     res.sendFile(path.join(__dirname, '..', 'index.html'));
+    console.log("File has been created");
+  });
+  res.sendFile(path.join(__dirname, '..', 'index.html'));
 });
 
 router.post('/image', function(req,res,next){   
 
-    var contents = fs.readFileSync(__dirname + "/images.json");
+  var contents = fs.readFileSync(__dirname + "/images.json");
     // Define to JSON type
     var jsonContent = JSON.parse(contents);
     // connect to MongoDB
     mongoose.Promise = global.Promise;
 
     mongoose.connect(config.database.connectionString)
-      .then(() =>  console.log('connection successful'))
-      .catch((err) => console.error(err));
+    .then(() =>  console.log('connection successful'))
+    .catch((err) => console.error(err));
 
     var image = new ImgurImage({
-        imgurId : req.body.id,
-        imgurLink : req.body.link
+      imgurId : req.body.id,
+      imgurLink : req.body.link
     });
     console.log("Image URL: " + image.imgurLink);
     if (image.imgurLink)
@@ -43,11 +43,11 @@ router.post('/image', function(req,res,next){
       jsonContent['images'].push(image.imgurLink);
     }
     fs.writeFile(__dirname + "/images.json", JSON.stringify(jsonContent, null, 4), (err) => {
-        if (err) {
-            console.error(err);
-            return;
-        };
-        console.log("File has been created");
+      if (err) {
+        console.error(err);
+        return;
+      };
+      console.log("File has been created");
     });
     // save the image into the database
     image.save(function(err) {
@@ -57,16 +57,6 @@ router.post('/image', function(req,res,next){
     res.send(jsonContent);
     console.log(jsonContent.images);
     mongoose.connection.close();    
-});
-/*
-router.put('/image', function(req, res, next){
-  console.log("Entered put");
-  console.log(req.body);
-   console.log("Stuff: " + req.body.faceAttributes);
-  //   for(var i = 0; i < data.length; i++){
-  //     happiness += data.faceAttributes.emotion.happiness[0];
-  //     console.log("Happiness: " + happiness);
-  // }
-});
-*/
+  });
+
 module.exports = router;
